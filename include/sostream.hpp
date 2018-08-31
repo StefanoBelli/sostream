b796966f5c1cdcb38d9d05bd78dae88856b02131
StefanoBelli <sa.belli@hotmail.it>
2018-08-31 16:42:00 +0200

    more

--- include/sostream.hpp
+++ include/sostream.hpp
@@ -24,122 +24,156 @@ namespace ssynx {
 
     namespace prot {
         struct tcp {
-            public:
-                static bool open(const char* hostname,
-                        std::uint16_t port, socket_resource_type* sock) noexcept;
-                static std::streamsize write(const char* data,
-                        std::streamsize datasize, socket_resource_type sock) noexcept;
-                static std::streamsize read_all(std::vector<char> &buffer, socket_resource_type sock);
-                static void close(socket_resource_type sock) noexcept;
+        public:
+            static bool open(const char *hostname,
+                             std::uint16_t port, socket_resource_type *sock) noexcept;
+
+            static std::streamsize write(const char *data,
+                                         std::streamsize datasize, socket_resource_type sock) noexcept;
+
+            static std::streamsize read_all(std::vector<char> &buffer, socket_resource_type sock);
+
+            static void close(socket_resource_type sock) noexcept;
+        };
+
+        struct udp {
+        public:
+            static bool open(const char *hostname,
+                             std::uint16_t port, socket_resource_type *sock) noexcept;
+
+            static std::streamsize write(const char *data,
+                                         std::streamsize datasize, socket_resource_type sock) noexcept;
+
+            static std::streamsize read_all(std::vector<char> &buffer, socket_resource_type sock);
+
+            static void close(socket_resource_type sock) noexcept;
+        };
+
+        struct ssl {
+        public:
+            static bool open(const char *hostname,
+                             std::uint16_t port, socket_resource_type *sock) noexcept;
+
+            static std::streamsize write(const char *data,
+                                         std::streamsize datasize, socket_resource_type sock) noexcept;
+
+            static std::streamsize read_all(std::vector<char> &buffer, socket_resource_type sock);
+
+            static void close(socket_resource_type sock) noexcept;
         };
     }
 
     namespace ipc {
 
-        template <typename ImplProvider,
-                 typename CharT,
-                 typename CharTraits = std::char_traits<CharT> >
-                     class basic_socketbuf : public std::basic_streambuf<CharT, CharTraits> {
-                         public:
-                             using char_type = CharT;
-                             using impl_type = ImplProvider;
-                             using int_type = typename CharTraits::int_type;
-                             using Base = std::basic_streambuf<CharT, CharTraits>;
+        template<typename ImplProvider,
+                typename CharT,
+                typename CharTraits = std::char_traits <CharT> >
+        class basic_socketbuf : public std::basic_streambuf<CharT, CharTraits> {
+        public:
+            using char_type = CharT;
+            using impl_type = ImplProvider;
+            using int_type = typename CharTraits::int_type;
+            using Base = std::basic_streambuf<CharT, CharTraits>;
 
-                             basic_socketbuf() {
+            basic_socketbuf() {
 #if defined(_WIN32)
-				 _Internal_WSA_Init();
+                _Internal_WSA_Init();
 #endif
-                                 char_type *get_buffer_data { get_buffer.data() };
-                                 Base::setg(get_buffer_data, get_buffer_data + 1, get_buffer_data + 1);
-                             }
+                char_type *get_buffer_data{get_buffer.data()};
+                Base::setg(get_buffer_data, get_buffer_data + 1, get_buffer_data + 1);
+            }
 
 #if defined(_WIN32)
-			     ~basic_socketbuf() {
-				 _Internal_WSA_Cleanup();
-			     }
+            ~basic_socketbuf() {
+            _Internal_WSA_Cleanup();
+            }
 #else
-			     ~basic_socketbuf() = default;
+
+            ~basic_socketbuf() = default;
+
 #endif
 
-                             bool open(const char* hostn, std::uint16_t port) noexcept {
-                                 return impl_type::open(hostn, port, &socket_res);
-                             }
+            bool open(const char *hostn, std::uint16_t port) noexcept {
+                return impl_type::open(hostn, port, &socket_res);
+            }
 
-                             void close() const noexcept {
-                                 impl_type::close(socket_res);
-                             }
+            void close() const noexcept {
+                impl_type::close(socket_res);
+            }
 
-                             std::streamsize xsputn(char_type const* data, std::streamsize size) override {
-                                 return impl_type::write(data, size, socket_res);
-                             }
+            std::streamsize xsputn(char_type const *data, std::streamsize size) override {
+                return impl_type::write(data, size, socket_res);
+            }
 
-                             int_type underflow() override {
-                                 std::fill(get_buffer.begin(), get_buffer.end(), 0);
+            int_type underflow() override {
+                std::fill(get_buffer.begin(), get_buffer.end(), 0);
 
-                                 std::streamsize read_size =
-                                     impl_type::read_all(static_cast<std::vector<char>&>(get_buffer), socket_res);
+                std::streamsize read_size =
+                        impl_type::read_all(static_cast<std::vector<char> &>(get_buffer), socket_res);
 
-                                 char_type *get_buffer_beginning { get_buffer.data() };
-				 Base::setg(get_buffer_beginning, get_buffer_beginning, get_buffer_beginning + read_size + 1);
+                char_type *get_buffer_beginning{get_buffer.data()};
+                Base::setg(get_buffer_beginning, get_buffer_beginning, get_buffer_beginning + read_size + 1);
 
-				 return CharTraits::to_int_type(get_buffer.size() ? get_buffer[0] : 0);
-                             }
+                if(read_size == 0)
+                    return CharTraits::to_int_type(0);
 
-                             socket_resource_type system_socket_resource() const noexcept {
-                                 return socket_res;
-                             }
+                return CharTraits::to_int_type(get_buffer[0]);
+            }
 
-                         private:
-                             std::vector<char_type> get_buffer	{ };
-                             socket_resource_type socket_res		{ };
-                     };
+            socket_resource_type system_socket_resource() const noexcept {
+                return socket_res;
+            }
 
-        template <typename ImplProvider,
-                 typename CharT,
-                 typename CharTraits = std::char_traits<CharT> >
-                     class basic_socketstream : public std::basic_iostream<CharT, CharTraits> {
-                         public:
-                             using char_type = CharT;
-                             using impl_type = ImplProvider;
-                             using Base = std::basic_iostream<CharT, CharTraits>;
-                             using BasicStreambuf = std::basic_streambuf<CharT, CharTraits>;
+        private:
+            std::vector <char_type> get_buffer{};
+            socket_resource_type socket_res{};
+        };
 
-                             basic_socketstream() : 
-				 Base(static_cast<BasicStreambuf*>(&underlying_buffer)) {}
+        template<typename ImplProvider,
+                typename CharT,
+                typename CharTraits = std::char_traits <CharT> >
+        class basic_socketstream : public std::basic_iostream<CharT, CharTraits> {
+        public:
+            using char_type = CharT;
+            using impl_type = ImplProvider;
+            using Base = std::basic_iostream<CharT, CharTraits>;
+            using BasicStreambuf = std::basic_streambuf<CharT, CharTraits>;
 
-                             basic_socketstream(const char* hostn, std::uint16_t port) :
-				 Base(static_cast<BasicStreambuf*>(&underlying_buffer)) {
+            basic_socketstream() :
+                    Base(static_cast<BasicStreambuf *>(&underlying_buffer)) {}
 
-				     open(hostn, port);
-                                 }
+            basic_socketstream(const char *hostn, std::uint16_t port) :
+                    Base(static_cast<BasicStreambuf *>(&underlying_buffer)) {
 
-                             ~basic_socketstream() {
-                                 close();
-                             }
+                open(hostn, port);
+            }
 
-                             bool open(const char* hostn, std::uint16_t port) noexcept {
-                                 bool rv = false;
+            ~basic_socketstream() {
+                close();
+            }
 
-                                 if(!(rv = underlying_buffer.open(hostn, port)))
-                                     Base::setstate(std::ios::failbit);
+            bool open(const char *hostn, std::uint16_t port) noexcept {
+                bool rv = false;
 
-                                 return rv;
-                             }
+                if (!(rv = underlying_buffer.open(hostn, port)))
+                    Base::setstate(std::ios::failbit);
 
-                             void close() noexcept {
-                                 underlying_buffer.close();
-                             }
+                return rv;
+            }
 
-                         private:
-                             basic_socketbuf<ImplProvider, CharT, CharTraits> underlying_buffer;
-                     };
+            void close() noexcept {
+                underlying_buffer.close();
+            }
+
+        private:
+            basic_socketbuf<ImplProvider, CharT, CharTraits> underlying_buffer;
+        };
 
         template<typename Impl>
-            using socketbuf = basic_socketbuf<Impl, char>;
+        using socketbuf = basic_socketbuf<Impl, char>;
 
         template<typename Impl>
-            using socketstream = basic_socketstream<Impl, char>;
+        using socketstream = basic_socketstream<Impl, char>;
 
     }
 
